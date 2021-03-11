1c821fac676c0ba212c5d536c716d4e138c128fb
aries <ariesaviles.dev@gmail.com>
2021-03-10 22:58:14 -0500

    Initial Commit

---
+++ hellomoon2/hellomoon2App.swift
@@ -0,0 +1,17 @@
+//
+//  hellomoon2App.swift
+//  hellomoon2
+//
+//  Created by aries on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct hellomoon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
